b358dc3bc6a5085299a9f9bb5ff734b6787e2209
DescrySama <erwing2002@gmail.com>
2022-02-14 15:25:40 +0100

    dernier commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-import confetti from 'canvas-confetti';
+    let temp = document.getElementById('temp');
+    let ville = prompt('choisissez le nom de votre ville : ')
+    let selectville = document.getElementById('ville')
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ville+'&appid=8f91983681d3b66b246d7478ad1d774a&units=metric';
+    let ajax = new XMLHttpRequest;
+    ajax.responseType = "json";
+    ajax.open("GET", url)
+    ajax.send();
+    
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+    ajax.addEventListener("readystatechange", function(){
+        if(ajax.readyState === ajax.DONE){
+            selectville.innerHTML = ville + ': ';
+            temp.innerHTML = ajax.response.main.temp + '°C';
+
+        }
+    })
+    
+
+
+    
+
+    
